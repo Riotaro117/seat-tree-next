808e355b1bb5ab1a0c2ff911d77508aa789536f0
riotaro <rugby117117@gmail.com>
2026-04-12 07:25:12 +0900

    レイアウトの作成機能

---
+++ app/api/layouts/create/route.ts
@@ -0,0 +1,36 @@
+import { supabase } from '@/lib/supabase';
+import type { ClassroomLayout, Seat, Student } from '@/lib/type';
+
+const createLayout = async (
+  userId: string,
+  layout: Omit<ClassroomLayout, 'id'>,
+): Promise<ClassroomLayout> => {
+  if (!userId) throw new Error('ユーザーIDが指定されていません。');
+  const { data, error } = await supabase
+    .from('layouts')
+    .insert({
+      user_id: userId, // 作成者
+      name: layout.name,
+      date: new Date().toISOString().split('T')[0],
+      rows: layout.rows,
+      cols: layout.cols,
+      seats: layout.seats,
+      students: layout.students,
+    })
+    .select()
+    .single();
+  if (error) throw new Error('現在の教室のレイアウトを保存できませんでした。', { cause: error });
+  if (!data) throw new Error('教室のレイアウトが存在しません。');
+  // appで使用するClassroomLayout型に変更
+  return {
+    id: data.id,
+    name: data.name,
+    date: data.date,
+    rows: data.rows,
+    cols: data.cols,
+    seats: data.seats as Seat[],
+    students: data.students as Student[],
+  };
+};
+
+export default createLayout;
