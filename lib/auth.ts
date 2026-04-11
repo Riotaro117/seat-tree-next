871dad45f7fa619ba33f22f07e2613b0acff282f
riotaro <rugby117117@gmail.com>
2026-04-12 05:34:54 +0900

    認証の監視イベントの作成

---
+++ lib/auth.ts
@@ -0,0 +1,15 @@
+import { User } from '@supabase/supabase-js';
+import { supabase } from './supabase';
+
+// 認証の監視イベント
+export const subscribeToAuthStateChange = (
+  onChange: (user: User | null | undefined) => void,
+): (() => void) => {
+  const {
+    data: { subscription },
+  } = supabase.auth.onAuthStateChange((_event, session) => {
+    onChange(session?.user ?? null);
+  });
+
+  return () => subscription.unsubscribe();
+};
