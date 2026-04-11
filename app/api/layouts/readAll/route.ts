fc0cfb375a49b575b6aecdf754d470a5bda242e0
riotaro <rugby117117@gmail.com>
2026-04-12 07:40:13 +0900

    教室レイアウトの読み込み機能作成

---
+++ app/api/layouts/readAll/route.ts
@@ -0,0 +1,26 @@
+import { supabase } from '@/lib/supabase';
+import type { ClassroomLayout, Seat, Student } from '@/lib/type';
+
+const fetchLayouts = async (userId: string): Promise<ClassroomLayout[]> => {
+  if (!userId) throw new Error('ユーザーIDが指定されていません。');
+
+  const { data, error } = await supabase
+    .from('layouts')
+    .select('*')
+    .eq('user_id', userId)
+    .order('created_at', { ascending: false })
+    .limit(20); // 存在しなければ[]を返し、nullは無い
+
+  if (error) throw new Error('教室のレイアウトの読み込みに失敗しました。', { cause: error });
+  if (!data) throw new Error('教室のレイアウトのデータがありません。');
+
+  // Json形式をClassroomLayout[]の型になるように変換
+  const formattedLayouts: ClassroomLayout[] = data.map((layout) => ({
+    ...layout,
+    seats: layout.seats as Seat[],
+    students: layout.students as Student[],
+  }));
+  return formattedLayouts;
+};
+
+export default fetchLayouts;
