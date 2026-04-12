cc5942466f7b0ecb536cc637f34bcf300d0a528b
riotaro <rugby117117@gmail.com>
2026-04-12 14:06:53 +0900

    生徒の更新の処理の作成

---
+++ app/api/students/update/route.ts
@@ -0,0 +1,27 @@
+import { formatStudent } from '@/lib/formatStudent.type';
+import { supabase } from '@/lib/supabase';
+import type { Student } from '@/lib/type';
+
+const updateStudent = async (student: Student, userId: string): Promise<Student> => {
+  if (!userId) throw new Error('ユーザーIDが指定されていません。');
+
+  const { data, error } = await supabase
+    .from('students')
+    .update({
+      name: student.name,
+      gender: student.gender,
+      needs_front_row: student.needsFrontRow,
+      bad_chemistry_with: student.badChemistryWith,
+    })
+    .eq('id', student.id)
+    .eq('user_id', userId)
+    .select()
+    .single();
+
+  if (error) throw new Error('生徒の更新に失敗しました。', { cause: error });
+  if (!data) throw new Error('生徒の情報が取得できませんでした。');
+
+  return formatStudent(data);
+};
+
+export default updateStudent;
