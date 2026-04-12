53d68c6c0585038868e0812f8b6b62286442efcc
riotaro <rugby117117@gmail.com>
2026-04-13 07:49:33 +0900

    isLoadingの変数の追加

--- app/providers/AuthProvider.tsx
+++ app/providers/AuthProvider.tsx
@@ -6,19 +6,21 @@ import { createContext, useContext, useEffect, useState } from 'react';
 
 type AuthContextType = {
   user: User | null | undefined;
+  isLoading: boolean;
 };
 
-const AuthContext = createContext<AuthContextType>({ user: undefined });
+const AuthContext = createContext<AuthContextType>({ user: undefined, isLoading: true });
 
 // childrenで小要素に渡す限り、子孫要素にクライアントコンポーネントが伝播することはない
 export const AuthProvider = ({ children }: { children: React.ReactNode }) => {
   const [user, setUser] = useState<User | null | undefined>(undefined); // 初期値はローディング中
+  const isLoading = user === undefined;
 
   useEffect(() => {
     const unsubscribe = subscribeToAuthStateChange(setUser);
     return unsubscribe;
   }, []);
-  return <AuthContext.Provider value={{ user }}>{children}</AuthContext.Provider>;
+  return <AuthContext.Provider value={{ user, isLoading }}>{children}</AuthContext.Provider>;
 };
 
 // hooksはContextから取得する形にする(グローバルステート化)
