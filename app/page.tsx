99f65d52ac1c4db1e3ea3d863faa0bf5f56aa6d3
riotaro <rugby117117@gmail.com>
2026-04-15 06:11:39 +0900

    トップページ更新

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,11 @@
-import Image from "next/image";
+import Header from './components/layouts/Header';
+import MainContents from './components/layouts/MainContents';
 
 export default function Home() {
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-    
-      </main>
+    <div className="min-h-screen bg-wood-50 text-wood-900 pb-20 font-sans">
+      <Header />
+      <MainContents />
     </div>
   );
 }
