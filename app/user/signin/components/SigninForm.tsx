b180093c03fdb7da338c5f8d7ef93d1590aef153
riotaro <rugby117117@gmail.com>
2026-04-16 07:42:42 +0900

    ページ遷移を追加

--- app/user/signin/components/SigninForm.tsx
+++ app/user/signin/components/SigninForm.tsx
@@ -1,19 +1,22 @@
-"use client"
+'use client';
 import { useState } from 'react';
 import signin from '@/app/api/user/signin/route';
 import { useAuthState } from '@/app/providers/AuthProvider';
 import Button from './Button';
+import { useRouter } from 'next/navigation';
 
 const SigninForm = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const { isLoading } = useAuthState();
 
+  const router = useRouter();
+
   const handleSignin = async (e: { preventDefault: () => void }) => {
     e.preventDefault();
     try {
       await signin(email, password);
-      console.log('ログイン成功');
+      router.push('/');
     } catch (error) {
       alert(error instanceof Error ? error.message : 'ログインに失敗しました。');
     }
@@ -37,7 +40,7 @@ const SigninForm = () => {
         placeholder="パスワード"
         className="w-full px-4 py-3 rounded-xl border-2 border-wood-100 focus:border-wood-400 outline-none bg-wood-50"
       />
-      <Button type="submit" disabled={!email || !password} isLoading={isLoading} color={"green"}>
+      <Button type="submit" disabled={!email || !password} isLoading={isLoading} color={'green'}>
         ログイン
       </Button>
     </form>
