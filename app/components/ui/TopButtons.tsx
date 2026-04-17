36f0f3385db9c9335bad9df7a81be694a4a2442b
riotaro <rugby117117@gmail.com>
2026-04-18 05:26:43 +0900

    リンクの追加

--- app/components/ui/TopButtons.tsx
+++ app/components/ui/TopButtons.tsx
@@ -1,12 +1,12 @@
-"use client"
+'use client';
 import { useAuthState } from '@/app/providers/AuthProvider';
 import { ImageIcon, Printer, Save, Settings, Shuffle, Users } from 'lucide-react';
+import Link from 'next/link';
 import type { UseReactToPrintFn } from 'react-to-print';
 
 interface TopButtonsProps {
   onRandomize: () => void;
   onTransitionSetting: () => void;
-  onTransitionStudentManager: () => void;
   onSaveCurrentLayout: () => Promise<void>;
   onPrintCurrentLayout: UseReactToPrintFn;
 }
@@ -14,11 +14,10 @@ interface TopButtonsProps {
 const TopButtons: React.FC<TopButtonsProps> = ({
   onRandomize,
   onTransitionSetting,
-  onTransitionStudentManager,
   onSaveCurrentLayout,
   onPrintCurrentLayout,
 }) => {
-  const {isLoading}=useAuthState()
+  const { isLoading } = useAuthState();
   return (
     <div className="flex flex-wrap gap-4 justify-center mb-8 sticky top-20 z-20 py-2 bg-wood-50/90 backdrop-blur-sm rounded-xl">
       <button
@@ -37,13 +36,13 @@ const TopButtons: React.FC<TopButtonsProps> = ({
         <Settings className="w-5 h-5" />
         座席設定
       </button>
-      <button
+      <Link
+        href={'/students/studentsRoster'}
         className="cursor-pointer items-center justify-center gap-2 px-4 py-2 rounded-xl font-bold transition-all duration-200 disabled:opacity-50 disabled:cursor-not-allowed shadow-sm active:scale-95 bg-white text-wood-800 border-2 border-wood-200 hover:border-wood-400 hover:bg-wood-50 shadow-md"
-        onClick={onTransitionStudentManager}
       >
         <Users className="w-5 h-5" />
         生徒名簿
-      </button>
+      </Link>
       <button
         className="cursor-pointer items-center justify-center gap-2 px-4 py-2 rounded-xl font-bold transition-all duration-200 disabled:opacity-50 disabled:cursor-not-allowed shadow-sm active:scale-95 bg-white text-wood-800 border-2 border-wood-200 hover:border-wood-400 hover:bg-wood-50 shadow-md"
         // onClick={() => setViewMode('history')}
