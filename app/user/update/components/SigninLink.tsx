8e1a5b3faac19e4552aa443c4d05f47227ea263a
riotaro <rugby117117@gmail.com>
2026-04-19 04:53:13 +0900

    すでにアカウントを持っている人へのリンクの切り出し

---
+++ app/user/update/components/SigninLink.tsx
@@ -0,0 +1,20 @@
+'use client';
+import signout from '@/app/api/user/signout/route';
+import { useRouter } from 'next/navigation';
+
+const SigninLink = () => {
+  const router = useRouter();
+  const handleLoadingSignin = async () => {
+    await signout();
+    router.replace('/user/signin');
+  };
+  return (
+    <div className="mt-4 text-center text-sm">
+      <button className="cursor-pointer w-full underline" onClick={handleLoadingSignin}>
+        すでにアカウントをお持ちの方
+      </button>
+    </div>
+  );
+};
+
+export default SigninLink;
