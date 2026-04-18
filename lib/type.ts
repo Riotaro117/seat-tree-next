7301dd72808907089c017594e05231e146782670
riotaro <rugby117117@gmail.com>
2026-04-18 12:30:41 +0900

    型の削除

--- lib/type.ts
+++ lib/type.ts
@@ -1,7 +1,5 @@
 // 仮UIを作成する前にデータ設計をする
 
-import type { ReactNode } from "react";
-
 // 先に型を作成して、それを元にDB(Supabase)、API(CRUD)、UI(React)を作成する
 export type Student = {
   id: string;
@@ -37,9 +35,3 @@ export type ClassroomLayout = {
 };
 
 export type ViewMode = 'classroom' | 'students' | 'history' | 'settings';
-
-export type Tab = {
-  id: number;
-  label: string;
-  content: ReactNode; // ReactNodeは文字列、JSX、コンポーネント、Fragment、okになる
-};
