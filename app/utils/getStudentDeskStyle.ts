59698591cc8642b8c431e535dc34e2047326a339
riotaro <rugby117117@gmail.com>
2026-04-23 06:24:13 +0900

    スタイル関数の作成

---
+++ app/utils/getStudentDeskStyle.ts
@@ -0,0 +1,46 @@
+import type { Seat, Student } from '@/lib/type';
+
+export const getStudentDeskStyle = (
+  student: Student | null,
+  seat?: Seat,
+  hasConflict?: boolean,
+) => {
+  if (seat?.isDisabled) {
+    return {
+      deskColor: 'bg-wood-100 border-wood-300 border-dashed cursor-not-allowed',
+      nameColor: 'text-wood-300',
+    };
+  } else if (!student) {
+    return {
+      deskColor: 'bg-wood-100 border-dashed border-wood-200',
+      nameColor: 'text-wood-300',
+    };
+  } else if (hasConflict) {
+    return {
+      deskColor: 'bg-yellow-200 border-yellow-300 animate-shake',
+      nameColor: 'text-red-900',
+    };
+  }
+
+  const styleMap = {
+    boy: {
+      deskColor: 'bg-sky-100 border-sky-200 hover:-translate-y-1 hover:shadow-lg',
+      nameColor: 'text-blue-900',
+    },
+    girl: {
+      deskColor: 'bg-rose-100 border-rose-200 hover:-translate-y-1 hover:shadow-lg',
+      nameColor: 'text-pink-900',
+    },
+    other: {
+      deskColor: 'bg-wood-100 border-dashed border-wood-400 hover:-translate-y-1 hover:shadow-lg',
+      nameColor: 'text-wood-300',
+    },
+  } as const; //リテラル型はその文字列しか入れられない
+
+  return (
+    styleMap[student.gender] ?? {
+      deskColor: 'bg-wood-100 border-dashed',
+      nameColor: 'text-wood-300',
+    }
+  );
+};
