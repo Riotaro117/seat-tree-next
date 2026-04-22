082a7b2d4e05eccc3bc681cb1c486c76309704a8
riotaro <rugby117117@gmail.com>
2026-04-23 06:22:23 +0900

    黒板の切り出し

---
+++ app/(with-header)/components/ui/BlackBoard.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const BlackBoard = () => {
+  return (
+    <div className="bg-lime-600 text-white px-12 py-2 rounded-b-xl shadow-md mb-8 w-2/3 text-center border-b-4 border-lime-800">
+      <h3 className="font-serif tracking-widest text-lg opacity-90">黒板</h3>
+    </div>
+  );
+};
+
+export default BlackBoard;
