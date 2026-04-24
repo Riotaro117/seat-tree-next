49e3c113e9ecb60c041dee85fab2088737cfac89
riotaro <rugby117117@gmail.com>
2026-04-25 06:15:41 +0900

    RouteHandlerの削除

--- app/api/user/signout/route.ts
+++
@@ -1,8 +0,0 @@
-import { createClient } from '@/lib/supabase/client';
-
-const signout = async (): Promise<void> => {
-  const { error } = await createClient().auth.signOut();
-  if (error) throw new Error('ログアウトできませんでした。',{cause:error});
-};
-
-export default signout;
