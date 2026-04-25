d0029775c9620d5bc0b2d0124b6cbf2919e22722
riotaro <rugby117117@gmail.com>
2026-04-26 06:46:43 +0900

    データ取得中の画面を作成

--- app/providers/DataProvider.tsx
+++ app/providers/DataProvider.tsx
@@ -1,19 +1,20 @@
 'use client';
 
-import { useEffect, useRef } from 'react';
+import { useEffect, useState } from 'react';
 import { useAuthState } from './AuthProvider';
 import { useStudentsStore } from '../store/useStudentsStore';
 import { useLayoutsStore } from '../store/useLayoutsStore';
 import { useSeatsStore } from '../store/useSeatsStore';
 import { fetchStudents, insertTemplateStudents } from '@/lib/supabase/students';
 import { fetchLayouts } from '@/lib/supabase/layouts';
+import SpinnerWhole from '../(with-header)/components/layouts/SpinnerWhole';
 
 export const DataProvider = ({ children }: { children: React.ReactNode }) => {
+  const [isDataLoading, setIsDateLoading] = useState(true); //初期状態をtrueにすることでデータ取得前のHTMLが見えなくなる
   const { isLoading, user } = useAuthState();
   const { setStudents } = useStudentsStore();
   const { setLayouts } = useLayoutsStore();
-  const { seats, handleResizeSeats,handleResizeCols } = useSeatsStore();
-  const hadInsertedTemplate = useRef(false); // 再レンダリングされても値が変わらないし、変更しても再レンダリングがトリガーされない
+  const { seats, handleResizeSeats, handleResizeCols } = useSeatsStore();
 
   useEffect(() => {
     if (!isLoading) {
@@ -23,13 +24,16 @@ export const DataProvider = ({ children }: { children: React.ReactNode }) => {
 
   // supabaseのデータ取得
   const fetchData = async () => {
-    if (!user) return null;
+    if (!user) {
+      setIsDateLoading(false);
+      return;
+    }
     try {
+      setIsDateLoading(true);
       // ①生徒情報を取得
       let formattedStudents = await fetchStudents(user.id);
       // ②匿名ユーザーであり生徒が０ならテンプレの投入
-      if (user.is_anonymous && formattedStudents.length === 0 && !hadInsertedTemplate.current) {
-        hadInsertedTemplate.current = true;
+      if (user.is_anonymous && formattedStudents.length === 0) {
         await insertTemplateStudents(user.id);
         // ③再取得
         formattedStudents = await fetchStudents(user.id);
@@ -45,6 +49,7 @@ export const DataProvider = ({ children }: { children: React.ReactNode }) => {
       console.error('Error fetching data:', error);
       alert('データの読み込みに失敗しました');
     } finally {
+      setIsDateLoading(false);
       if (seats.length === 0) {
         handleResizeSeats(30);
         handleResizeCols(6, 30);
@@ -52,5 +57,5 @@ export const DataProvider = ({ children }: { children: React.ReactNode }) => {
     }
   };
 
-  return <>{children}</>;
+  return <>{isLoading || isDataLoading ? <SpinnerWhole /> : children}</>;
 };
