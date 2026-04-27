036e799d9b1ea37ab52ebd51efa56c636708f005
riotaro <rugby117117@gmail.com>
2026-04-28 05:46:48 +0900

    ファイルの位置の変更

--- app/(with-header)/components/layouts/Spinner.tsx
+++
@@ -1,7 +0,0 @@
-import { Loader2 } from 'lucide-react';
-
-const Spinner = () => {
-  return <Loader2 className="w-5 h-5 animate-spin text-center mx-auto" />;
-};
-
-export default Spinner;
--- app/(with-header)/page.tsx
+++
@@ -1,5 +0,0 @@
-import MainContents from "./components/layouts/MainContents";
-
-export default function Home() {
-  return <MainContents />;
-}
