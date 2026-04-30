97105283ede57891c9261bfa52acdc5191bf0954
riotaro <rugby117117@gmail.com>
2026-05-01 06:03:58 +0900

    DataProviderの追加

--- app/classroom/layout.tsx
+++ app/classroom/layout.tsx
@@ -1,10 +1,13 @@
-import Header from "./components/layouts/Header";
+import { DataProvider } from '../providers/DataProvider';
+import Header from './components/layouts/Header';
 
 const WithHeaderLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="min-h-screen bg-wood-50 text-wood-900 pb-20 font-sans">
       <Header />
-      <main className="max-w-7xl mx-auto px-4 pt-6">{children}</main>
+      <DataProvider>
+        <main className="max-w-7xl mx-auto px-4 pt-6">{children}</main>
+      </DataProvider>
     </div>
   );
 };
