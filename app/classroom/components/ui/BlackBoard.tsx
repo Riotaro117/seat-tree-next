1570ee68b37ca29fd302d5a9fd5524e096128843
riotaro <rugby117117@gmail.com>
2026-05-14 10:17:44 +0900

    黒板のスタイルの変更

--- app/classroom/components/ui/BlackBoard.tsx
+++ app/classroom/components/ui/BlackBoard.tsx
@@ -1,7 +1,7 @@
 const BlackBoard = () => {
   return (
-    <div className="bg-lime-600 text-white px-12 py-2 rounded-xl shadow-md mb-8 w-2/3 text-center border-b-4 border-lime-700">
-      <h3 className="tracking-widest text-lg">黒板</h3>
+    <div className="bg-emerald-600 text-white px-12 py-2 rounded-xl shadow-md mb-8 w-2/3 text-center border-b-4 border-emerald-700">
+      <h3 className="tracking-widest text-lg font-serif">黒板</h3>
     </div>
   );
 };
