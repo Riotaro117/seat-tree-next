87d317c250dcb1fd988f756f34c19321ef28e0d8
riotaro <rugby117117@gmail.com>
2026-05-14 15:24:27 +0900

    マジックナンバーの追加

--- app/utils/seatingLogics/constants.ts
+++ app/utils/seatingLogics/constants.ts
@@ -1,6 +1,13 @@
+// 隣の座席の相対位置
 export const ADJACENT_OFFSETS = [
-  { r: 0, c: -1 }, // 左
-  { r: 0, c: 1 }, // 右
-  { r: -1, c: 0 }, // 上
-  { r: 1, c: 0 }, // 下
+  { r: 0, c: -1 }, // 左隣
+  { r: 0, c: 1 }, // 右隣
+  { r: -1, c: 0 }, // 上隣
+  { r: 1, c: 0 }, // 下隣
 ];
+
+// 500回ランダムに席替えをして、スコアが最小のものを見つける
+export const ATTEMPTS = 500;
+
+// 席替えの違反の重み
+export const HARD_CONFLICT_WEIGHT = 1000;
